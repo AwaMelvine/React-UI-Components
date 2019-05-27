071152d70e263bb29c81f1d3dcf27922c08d4c10
Awa Melvine <melvineawa@gmail.com>
2019-05-27 12:35:53 +0100

    Create header title component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = () => (
+  <div className="header-title">
+    <h3>Lambda School</h3>
+    <span>@LambdaSchool</span>
+    <span>.</span>
+    <span>26 Jan</span>
+  </div>
+);
+
+export default HeaderTitle;
