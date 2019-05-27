89f5d6911a156035e9216d2d84fdab20e3a6e518
Awa Melvine <melvineawa@gmail.com>
2019-05-27 12:46:50 +0100

    Create header container componenet

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,10 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import HeaderContent from "./HeaderContent";
+
+const HeaderContainer = () => (
+  <div className="header-container">
+    <HeaderContent />
+  </div>
+);
+export default HeaderContainer;
