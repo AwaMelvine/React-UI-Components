962c09cf225721f6e200a387d0b7daf59678c510
Awa Melvine <melvineawa@gmail.com>
2019-05-27 14:46:58 +0100

    Create card summary component

---
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardSummary.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Card.css";
+
+const CardSummary = () => (
+  <div className="card-summary">
+    <h3>Get started with React</h3>
+    <p>
+      React makes it painless to create interactive UIs. Design simple views for
+      each state in your application.
+    </p>
+    <a href="https://www.reactjs.org">reactjs.org</a>
+  </div>
+);
+
+export default CardSummary;
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Header.css";
 import HeaderContent from "./HeaderContent";
+import ImageThumbnail from "./ImageThumbnail";
 
 const HeaderContainer = () => (
   <div className="header-container">
+    <ImageThumbnail />
     <HeaderContent />
   </div>
 );
