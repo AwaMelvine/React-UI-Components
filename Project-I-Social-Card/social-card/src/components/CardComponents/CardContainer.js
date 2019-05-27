c9e7b0ec9f684a130da364d778961d56cb25c4c6
Awa Melvine <melvineawa@gmail.com>
2019-05-27 14:53:52 +0100

    Import all sub components of app into app

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,11 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+import CardContent from "./CardContent";
+
+const CardContainer = () => (
+  <div>
+    <CardContent />
+  </div>
+);
+
+export default CardContainer;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,13 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
+import CardBanner from "./CardBanner";
+import CardSummary from "./CardSummary";
+
+const CardContent = () => (
+  <div className="card-content">
+    <CardBanner />
+    <CardSummary />
+  </div>
+);
+
+export default CardContent;
