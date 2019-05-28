df6ef6e38a3c43ec0e636929c6bec2ea0d91d99c
Awa Melvine <melvineawa@gmail.com>
2019-05-28 12:57:53 +0100

    Create calculator display component

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
-import React from 'react';
-import './Display.css';
+import React from "react";
+import "./Display.css";
+
+const CalculatorDispplay = ({ value = 0 }) => (
+  <div className="calculator-display">
+    <input name="displayValue" value={value} />
+  </div>
+);
+
+export default CalculatorDispplay;
