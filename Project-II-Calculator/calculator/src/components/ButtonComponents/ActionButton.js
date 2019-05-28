f626230eb14a6d84ab47e3605f36ca3e9b81fede
Awa Melvine <melvineawa@gmail.com>
2019-05-28 13:32:31 +0100

    Create action button comp onent

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
+import "./Button.css";
+
+const ActionButton = ({ label }) => (
+  <React.Fragment>
+    <button className="action-button">{label}</button>
+  </React.Fragment>
+);
+
+export default ActionButton;
